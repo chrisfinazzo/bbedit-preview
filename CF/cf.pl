40727e64b356583a0d272c4e10493f3b35ea32ad
chrisfinazzo <chris.finazzo@gmail.com>
2026-05-26 21:12:28 -0400

    Revert to the simplified filter...for now

--- CF/cf.pl
+++ CF/cf.pl
@@ -1,34 +1,22 @@
-#!/usr/bin/env perl
+#!/usr/bin/perl -n
 
-use strict;
-use warnings;
+BEGIN {
+    $i = 0;
+    $in_frontmatter = 0;
+    open(HTML_PIPE, "| cmark");
+}
 
-# BBEdit filters read from stdin and write to stdout
-# Variables to track YAML frontmatter
-my $in_frontmatter = 0;
-my $frontmatter_found = 0;
-my $line_num = 0;
-
-# Process input line by line
-while (my $line = <STDIN>) {
-    $line_num++;
-    
-    # Check for YAML frontmatter start
-    if ($line_num == 1 && $line =~ /^---\s*$/) {
-        $in_frontmatter = 1;
-        $frontmatter_found = 1;
-        next;
-    }
-    
-    # Check for YAML frontmatter end
-    if ($in_frontmatter && $line =~ /^---\s*$/) {
+if ($in_frontmatter) {
+    if (/^---/) {
         $in_frontmatter = 0;
-        next;
     }
-    
-    # Skip lines within frontmatter
-    next if $in_frontmatter;
-    
-    # Output regular content to stdout
-    print $line;
+} elsif (/^---/ && $i < 2) {
+    $i++;
+    $in_frontmatter = 1;
+} else {
+    print HTML_PIPE $_;
+}
+
+END {
+    close(HTML_PIPE);
 }
